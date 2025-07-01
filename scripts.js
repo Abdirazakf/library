17bf6c68eeb83173b409822077611de9a6cb7cab
Abdirazakf <farahabdirazak13@gmail.com>
2025-07-01 17:35:48 -0400

    Taking in the form information when the submit button is clicked and stored that into an object

--- scripts.js
+++ scripts.js
@@ -1,11 +1,43 @@
+const form = document.querySelector("form")
+const card = document.querySelector(".card")
 const addBook = document.querySelector(".add-card")
-const bookModal = document.querySelector("dialog")
+const addBookModal = document.querySelector("dialog")
+const submitButton = document.querySelector(".submit-button")
 const closeButton = document.querySelector(".close-button")
 
+form.addEventListener("submit",(event) =>{
+    event.preventDefault();
+    addBookModal.close();
+    addBookToLibrary();
+    form.reset();
+})
+
 addBook.addEventListener("click",() =>{
-    bookModal.showModal();
+    addBookModal.showModal();
 })
 
 closeButton.addEventListener("click",() =>{
-    bookModal.close()
+    addBookModal.close();
 })
+
+const myLibrary = []
+
+function Book(title,author,pages,book_url,read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.book_url = book_url
+    this.read = read
+}
+
+function addBookToLibrary(){
+    let title = document.querySelector("input[id='title'").value
+    let author = document.querySelector("input[id='author'").value
+    let pages = document.querySelector("input[id='pages'").value
+    let book_url = document.querySelector("input[id='book-cover'").value
+    let read = document.querySelector("input[id='read'").checked
+    
+    const newBook = new Book(title,author,pages,book_url,read)
+    
+    console.log(newBook)
+}
