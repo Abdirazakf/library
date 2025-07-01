ae0970f7930f2f8b172955d3f4e34f1c8357cdd4
Abdirazakf <farahabdirazak13@gmail.com>
2025-07-01 17:39:36 -0400

    Stored the book objects into the myLibrary array

--- scripts.js
+++ scripts.js
@@ -10,34 +10,35 @@ form.addEventListener("submit",(event) =>{
     addBookModal.close();
     addBookToLibrary();
     form.reset();
-})
+});
 
 addBook.addEventListener("click",() =>{
     addBookModal.showModal();
-})
+});
 
 closeButton.addEventListener("click",() =>{
     addBookModal.close();
-})
+    form.reset();
+});
 
-const myLibrary = []
+const myLibrary = [];
 
 function Book(title,author,pages,book_url,read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.book_url = book_url
-    this.read = read
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.book_url = book_url;
+    this.read = read;
 }
 
 function addBookToLibrary(){
-    let title = document.querySelector("input[id='title'").value
-    let author = document.querySelector("input[id='author'").value
-    let pages = document.querySelector("input[id='pages'").value
-    let book_url = document.querySelector("input[id='book-cover'").value
-    let read = document.querySelector("input[id='read'").checked
+    let title = document.querySelector("input[id='title'").value;
+    let author = document.querySelector("input[id='author'").value;
+    let pages = document.querySelector("input[id='pages'").value;
+    let book_url = document.querySelector("input[id='book-cover'").value;
+    let read = document.querySelector("input[id='read'").checked;
     
-    const newBook = new Book(title,author,pages,book_url,read)
-    
-    console.log(newBook)
+    const newBook = new Book(title,author,pages,book_url,read);
+    myLibrary.push(newBook);
+    console.log(myLibrary);
 }
